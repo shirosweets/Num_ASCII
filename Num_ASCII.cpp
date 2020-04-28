c5f2f941935660ebfce5cb56e41672bee0eab9c3
shirosweets <60824319+shirosweets@users.noreply.github.com>
2020-04-28 16:54:53 -0300

    Add files via upload

---
+++ Num_ASCII.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	int i;
+	cout<< "ingrese un numero entero" << endl;
+	cin>> a;
+	i=a;
+	cout<< "el codigo ASCII es: " <<i << endl;
+	
+}
+
